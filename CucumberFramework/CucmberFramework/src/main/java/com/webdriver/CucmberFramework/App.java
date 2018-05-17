d474601f04701adcb0aa12a0c5c4f6984335de3d
Sairam Sivaswami <ss_sairam@yahoo.com>
2018-05-18 08:41:57 +1200

    Basic skeleton of the framework
    issue: Create a skeleton
    reviewer:

---
+++ CucumberFramework/CucmberFramework/src/main/java/com/webdriver/CucmberFramework/App.java
@@ -0,0 +1,13 @@
+package com.webdriver.CucmberFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ CucumberFramework/CucmberFramework/src/test/java/com/webdriver/CucmberFramework/StepDefinition/LoginStepDefinition.java
@@ -0,0 +1,35 @@
+package com.webdriver.CucmberFramework.StepDefinition;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginStepDefinition {
+	
+	@Given("^the user is on the login page$")
+	public void the_user_is_on_the_login_page() throws Throwable {
+System.out.println("the_user_is_on_the_login_page");
+	}
+
+	@Given("^the user enters a valid username$")
+	public void the_user_enters_a_valid_username() throws Throwable {
+		System.out.println("the_user_enters_a_valid_username");
+	}
+
+	@Given("^the user enters a valid password$")
+	public void the_user_enters_a_valid_password() throws Throwable {
+		System.out.println("the_user_enters_a_valid_password");
+	}
+
+	@When("^the user clicks on the login button$")
+	public void the_user_clicks_on_the_login_button() throws Throwable {
+		System.out.println("the_user_clicks_on_the_login_button");
+	}
+
+	@Then("^the user is redirected to the successful login page$")
+	public void the_user_is_redirected_to_the_successful_login_page() throws Throwable {
+		System.out.println("the_user_is_redirected_to_the_successful_login_page");
+	}
+
+}
